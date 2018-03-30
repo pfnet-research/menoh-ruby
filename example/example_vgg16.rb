bc43970af895ef9609edeeb135f4bcc8c7cb5ee0
Kunihiko MIYOSHI <miyoshik@preferred.jp>
2018-03-30 15:16:59 +0900

    add resource downloader for VGG16 example

--- example/example_vgg16.rb
+++ example/example_vgg16.rb
@@ -1,10 +1,26 @@
+require 'open-uri'
 require 'rmagick'
 include Magick
 
-require 'runx' # TODO
+require 'runx'
+
+# download dependencies
+def download_file(url, output)
+  return if File.exist? output
+  puts "downloading... #{url}"
+  open(output, 'wb') do |f_output|
+    open(url, 'rb') do |f_input|
+      f_output.write f_input.read
+    end
+  end
+end
+download_file("https://www.dropbox.com/s/bjfn9kehukpbmcm/VGG16.onnx?dl=1", "./data/VGG16.onnx")
+download_file("https://raw.githubusercontent.com/HoldenCaulfieldRye/caffe/master/data/ilsvrc12/synset_words.txt", "./data/synset_words.txt")
+download_file("https://upload.wikimedia.org/wikipedia/commons/5/54/Light_sussex_hen.jpg", "./data/Light_sussex_hen.jpg")
+download_file("https://upload.wikimedia.org/wikipedia/commons/f/fd/FoS20162016_0625_151036AA_%2827826100631%29.jpg", "./data/honda_nsx.jpg")
 
 # load ONNX file
-onnx_obj = Runx::Runx.new("./data/VGG16.onnx") # TODO
+onnx_obj = Runx::Runx.new("./data/VGG16.onnx")
 
 CONV1_1_IN_NAME = "140326425860192"
 FC6_OUT_NAME = "140326200777584"
@@ -13,6 +29,7 @@ SOFTMAX_OUT_NAME = "140326200803680"
 # load dataset
 imagelist = [
   "./data/Light_sussex_hen.jpg",
+  "./data/honda_nsx.jpg",
 ]
 
 # conditions for inference
