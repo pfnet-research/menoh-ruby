6d5d2a5f65e1933c150f218efd6223b8ef5b9fd0
Masahiro Sakai <masahiro.sakai@gmail.com>
2018-11-02 12:02:00 +0900

    add -std= option in extconf.rb
    This is because old GCC uses -std=gnu90 as a default.

--- ext/menoh_native/extconf.rb
+++ ext/menoh_native/extconf.rb
@@ -1,5 +1,15 @@
 require 'mkmf'
 
+if try_cflags '-std=gnu11'
+  $CFLAGS += " -std=gnu11"
+elsif try_cflags '-std=c11'
+  $CFLAGS += " -std=c11"
+elsif try_cflags '-std=gnu99'
+  $CFLAGS += " -std=gnu99"
+elsif try_cflags '-std=c99'
+  $CFLAGS += " -std=c99"
+end
+
 if pkg_config("menoh")
   create_makefile('menoh/menoh_native')
 end
